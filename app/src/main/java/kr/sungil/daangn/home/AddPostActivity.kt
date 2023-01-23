0ad43ebf5332968fc5866773029bcaed18594c1c
박원준 <upj53@sungil-i.kr>
2023-01-23 12:15:25 +0900

    done

--- app/src/main/java/kr/sungil/daangn/home/AddPostActivity.kt
+++ app/src/main/java/kr/sungil/daangn/home/AddPostActivity.kt
@@ -35,37 +35,42 @@ class AddPostActivity : AppCompatActivity() {
 		setContentView(binding.root)
 
 		initEditText()
-		initPhotoUploadButton()
+		initImageUploadButton()
 		initSubmitButton()
 		initCancelButton()
 	}
 
 	private fun initEditText() {
+		// EditText(제목, 가격) 값이 있을 때만 등록 버튼을 활성화 합니다.
 		binding.apply {
 			etTitle.addTextChangedListener {
-				val enable = etTitle.text.isNotEmpty() && etPrice.text.isNotEmpty()
+				val enable = etTitle.text.trim().isNotEmpty() && etPrice.text.trim().isNotEmpty()
 				btSubmit.isEnabled = enable
 			}
 			etPrice.addTextChangedListener {
-				val enable = etTitle.text.isNotEmpty() && etPrice.text.isNotEmpty()
+				val enable = etTitle.text.trim().isNotEmpty() && etPrice.text.trim().isNotEmpty()
 				btSubmit.isEnabled = enable
 			}
 		}
 	}
 
-	private fun initPhotoUploadButton() {
+	private fun initImageUploadButton() {
 		binding.apply {
 			btUploadImage.setOnClickListener {
+				// 저장소 읽기 권한 요청
 				when {
 					ContextCompat.checkSelfPermission(
 						applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE
-					) == PackageManager.PERMISSION_GRANTED -> {
+					) == PackageManager.PERMISSION_GRANTED -> { // 권한이 있을 경우
+						// ContextProvider 를 실행시킵니다.
 						startContentProvider()
 					}
-					shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+					shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> { // 권한 요청을 합니다
+						// Popup 창을 띄워 권한을 요청합니다
 						showPermissionContextPopup()
 					}
-					else -> {
+					else -> { // 권한 요청
+						// 권한을 요청합니다
 						requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1010)
 					}
 				}
@@ -74,12 +79,19 @@ class AddPostActivity : AppCompatActivity() {
 	}
 
 	private fun initSubmitButton() {
+		// 새 글 등록 버튼 이벤트
 		binding.apply {
+			// 처음에는 버튼을 비활성화 합니다.
+			btSubmit.isEnabled = false
 			btSubmit.setOnClickListener {
-				val sellerId = AUTH.currentUser?.uid.orEmpty()
-				val title = etTitle.text.toString()
-				val price = etPrice.text.toString().toInt()
+				// 로그인이 되어 있는지 체크합니다
+				if (AUTH.currentUser == null) return@setOnClickListener
 
+				val sellerId = AUTH.currentUser?.uid.orEmpty()
+				val title = etTitle.text.toString().trim()
+				val price = etPrice.text.toString().trim().toInt()
+
+				// Progress 창을 보여줍니다.
 				showProgress()
 
 				// 이미지가 있으면 업로드 과정을 추가합니다.
@@ -104,23 +116,27 @@ class AddPostActivity : AppCompatActivity() {
 	}
 
 	private fun initCancelButton() {
+		// 취소 버튼 이벤트
 		binding.ivClose.setOnClickListener {
 			finish()
 		}
 	}
 
 	private fun uploadPost(sellerId: String, title: String, price: Int, imageUrl: String) {
+		// PostModel 객체를 생성합니다
 		val postModel = PostModel(sellerId, title, System.currentTimeMillis(), price, imageUrl)
+		// Firebase Database 에 저장합니다
 		postDB.push().setValue(postModel)
 
 		Toast.makeText(
 			applicationContext, getString(R.string.post_submit_ok), Toast.LENGTH_LONG
 		).show()
 		hideProgress()
-//		finish()
+		finish()
 	}
 
 	private fun uploadImage(uri: Uri, successHandler: (String) -> Unit, errorHandler: () -> Unit) {
+		// 파일 이름을 임의로 지정합니다.
 		val fileName = "${System.currentTimeMillis()}.png"
 		storage.reference.child(STORAGE_IMAGE).child(fileName).putFile(uri).addOnCompleteListener {
 			if (it.isSuccessful) { // 이미지 업로드 성공
@@ -140,17 +156,44 @@ class AddPostActivity : AppCompatActivity() {
 	}
 
 	private fun showProgress() {
+		// 업로드 중에 보여줄 Progress 를 보여줍니다.
 		binding.pbUploadProgress.isVisible = true
 	}
 
 	private fun hideProgress() {
+		// 업로드 중에 보여줄 Progress 를 없앱니다..
 		binding.pbUploadProgress.isVisible = false
 	}
 
 	private fun startContentProvider() {
+		// ContentProvider 로 저장소의 파일을 열람합니다.
 		val intent = Intent(Intent.ACTION_GET_CONTENT)
 		intent.type = "image/*"
 		startActivityForResult(intent, 2020)
+	}
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		if (resultCode != Activity.RESULT_OK) {
+			return
+		}
+
+		when (requestCode) {
+			2020 -> {
+				val uri = data?.data
+				if (uri != null) {
+					// 이미지를 성공적으로 읽었을 경우 이미지 미리보기를 합니다.
+					binding.ivPhoto.setImageURI(uri)
+					selectedUri = uri
+				} else {
+					Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+				}
+
+			}
+			else -> {
+				Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+			}
+		}
 	}
 
 	private fun showPermissionContextPopup() {
@@ -173,26 +216,10 @@ class AddPostActivity : AppCompatActivity() {
 		}
 	}
 
-	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-		super.onActivityResult(requestCode, resultCode, data)
-		if (resultCode != Activity.RESULT_OK) {
-			return
-		}
+	override fun onStart() {
+		super.onStart()
 
-		when (requestCode) {
-			2020 -> {
-				val uri = data?.data
-				if (uri != null) {
-					binding.ivPhoto.setImageURI(uri)
-					selectedUri = uri
-				} else {
-					Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
-				}
-
-			}
-			else -> {
-				Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
-			}
-		}
+		// 로그인이 되어 있는지 체크합니다
+		if (AUTH.currentUser == null) finish()
 	}
 }
